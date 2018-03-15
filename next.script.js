75143b4bb1f4addf4fdbd82f6416c2ead89668b2
Ovyerus <iamovyerus@gmail.com>
2018-03-15 18:14:24 +1100

    Add dialogue adding

--- next.script.js
+++ next.script.js
@@ -5,6 +5,12 @@ const categories = [
 	'Trivia', 'Writing', 'You'
 ];
 
+/**
+ * Converts a HTML string into an element.
+ * 
+ * @param {String} html HTML string to convert.
+ * @returns {HTMLElement} Generated element.
+ */
 function htmlToEl(html) {
 	let template = document.createElement('template');
     html = html.trim(); // Never return a text node of whitespace as the result
@@ -14,16 +20,17 @@ function htmlToEl(html) {
 }
 
 function clean(str, space) {
-	str = str.replace(/[^\w\s]|_/g, "");
-	str = space ? str : str.replace(/ /g, "_");
+	let ret = str.replace(/[^\w\s]|_/g, "");
+	ret = space ? ret : ret.replace(/ /g, "_");
+
 	return str.toLowerCase();
 }
 
 function addCategory() {
-	let dropper = htmlToEl('<div class="control pad-bottom"><span class="select"><select></select></span></div>'); // Create bare. dropdown element tree.
-	let select = dropper.firstChild.firstChild; // Get a reference to the `<select>` element.
+	let dropper = htmlToEl('<div class="field is-grouped pad-bottom"><div class="control"><span class="select"><select></select></span></div></div>'); // Create bare. dropdown element tree.
+	let deleteButton = htmlToEl('<div class="control"><button class="button is-danger"><span class="icon is-small"><i class="fas fa-times"></i></span></button></div>'); // Generate the delete button.
+	let select = dropper.firstChild.firstChild.firstChild; // Get a reference to the `<select>` element.
 
-	dropper.appendChild(htmlToEl('<button class="button is-danger" style="margin-left: 0.2em !important;"><span class="icon is-small"><i class="fas fa-times"></i></span></button>')); // Add the remove button to the tree.
 	categories.forEach(el => {
 		let cat = document.createElement('option');
 		cat.innerHTML = el;
@@ -32,10 +39,33 @@ function addCategory() {
 		select.appendChild(cat);
 	});
 
-	dropper.lastChild.setAttribute('onclick', 'removeCategory(this)');
+	deleteButton.setAttribute('onclick', 'removeEl(this, "categories")');
+	dropper.appendChild(deleteButton); // Add the remove button to the tree.
 	document.getElementById('categories').appendChild(dropper);
 }
 
-function removeCategory(el) {
-	document.getElementById('categories').removeChild(el.parentElement);
+function addDialogue() {
+	// Generate base field, and generate buttons separately.
+	let field = htmlToEl('<div class="field is-grouped pad-bottom"><span class="control is-expanded"><input type="text" class="input" placeholder="An example line of dialogue."></span></div>');
+	let poseButton = htmlToEl('<span class="control"><button class="button is-primary">Pose</button></span>');
+	let faceButton = htmlToEl('<span class="control"><button class="button is-primary">Face</button></span>');
+	let deleteButton = htmlToEl('<span class="control"><button class="button is-danger"><span class="icon"><i class="fas fa-times"></i></span></button></span>');
+
+	// poseButton.setAttribute('onclick', 'selectPose(this)');
+	// faceButton.setAttribute('onclick', 'selectFace(this)');
+	deleteButton.setAttribute('onclick', 'removeEl(this, "dialogue")');
+
+	field.appendChild(poseButton);
+	field.appendChild(faceButton);
+	field.appendChild(deleteButton);
+	document.getElementById('dialogue').appendChild(field);
 }
+
+function removeEl(el, container) {
+	document.getElementById(container).removeChild(el.parentElement);
+}
+
+function selectPose(el) {}
+function selectFace(el) {}
+
+// https://stackoverflow.com/questions/4941004/putting-images-with-options-in-a-dropdown-list
